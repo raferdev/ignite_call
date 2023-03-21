123689dc3d8d436790677cfc4dd28a9c439e3418
Rafael <rafinha_fern@hotmail.com>
2023-03-21 16:29:07 -0300

    first commit: Next setup!

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,17 @@
+import { getCssText } from '@ignite-ui/react'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
+
+      <style id='sitches' dangerouslySetInnerHTML={{__html:getCssText()}}/>
+      </Head>
       <body>
         <Main />
         <NextScript />

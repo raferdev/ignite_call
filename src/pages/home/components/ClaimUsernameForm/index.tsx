863cb15e9ca80e1cf6fd7977a30d6cc914b10501
Rafael <rafinha_fern@hotmail.com>
2023-03-25 00:22:03 -0300

    feat: add zod, react hookform, and hookform resolver to home page form.

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,11 +1,28 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from '@phosphor-icons/react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Form } from './styles'
 
+const claimUserNameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUserNameFormData = z.infer<typeof claimUserNameFormSchema>
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUserNameFormData>()
+
+  async function handlerClaimUsername(data: ClaimUserNameFormData) {}
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="your-user" />
+    <Form as="form" onSubmit={handleSubmit(handlerClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="your-user"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Schedule
         <ArrowRight />

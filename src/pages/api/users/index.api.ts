e6199eeacc9739427414156bdbab125613eaaa7f
Rafael <rafinha_fern@hotmail.com>
2023-03-29 17:46:24 -0300

    feat: user persist using cookies, nookies lib.

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,6 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+import { setCookie } from 'nookies'
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
@@ -11,11 +11,29 @@ export default async function handler(
 
   const { name, username } = req.body
 
+  const uniqueUsername = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (uniqueUsername) {
+    return res.status(400).json({
+      message: 'Username already exist.',
+    })
+  }
+
   const user = await prisma.user.create({
     data: {
       name,
       username,
     },
+  })
+
+  const MAXAGE_COOKIE = 60 * 60 * 24 * 7 // 7 days on seconds
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: MAXAGE_COOKIE,
+    path: '/',
   })
 
   return res.status(201).json(user)

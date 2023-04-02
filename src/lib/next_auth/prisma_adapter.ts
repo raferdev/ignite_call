3fed090053658890617fd17a39ff5e4cda027aa6
Rafael <rafinha_fern@hotmail.com>
2023-04-01 22:13:30 -0300

    feat: new getUser method on next auth lib.

---
+++ src/lib/next_auth/prisma_adapter.ts
@@ -0,0 +1,36 @@
+import { Adapter } from 'next-auth/adapters'
+import { prisma } from '../prisma'
+
+export function PrismaAdapter(): Adapter {
+  return {
+    async createUser(user) {},
+
+    async getUser(id) {
+      const user = await prisma.user.findUniqueOrThrow({
+        where: {
+          id,
+        },
+      })
+      return {
+        id: user?.id,
+        name: user?.name,
+        username: user?.username,
+        emailVerified: null,
+        email: user?.email!,
+        avatar_url: user?.avatar_url!,
+      }
+    },
+    async getUserByEmail(email) {},
+    async getUserByAccount({ providerAccountId, provider }) {},
+    async updateUser(user) {},
+    async deleteUser(userId) {},
+    async linkAccount(account) {},
+    async unlinkAccount({ providerAccountId, provider }) {},
+    async createSession({ sessionToken, userId, expires }) {},
+    async getSessionAndUser(sessionToken) {},
+    async updateSession({ sessionToken }) {},
+    async deleteSession(sessionToken) {},
+    async createVerificationToken({ identifier, expires, token }) {},
+    async useVerificationToken({ identifier, token }) {},
+  }
+}

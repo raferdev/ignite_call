69ebf165d2c4d467e79ec2d521777e5700efa073
Rafael <rafinha_fern@hotmail.com>
2023-04-03 09:54:08 -0300

    feat: new time string to minutes utils, zod transform form data.

--- src/pages/register/time_intervals/index.page.tsx
+++ src/pages/register/time_intervals/index.page.tsx
@@ -19,6 +19,7 @@ import {
   IntervalItem,
 } from './styles'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { convertTimeStringToMinutes } from '@/utils/convert_time_to_min.js'
 
 const timeIntervalsFormSchema = z.object({
   intervals: z
@@ -34,10 +35,31 @@ const timeIntervalsFormSchema = z.object({
     .transform((intervals) => intervals.filter((interval) => interval.enabled))
     .refine((intervals) => intervals.length > 0, {
       message: 'You need enable 1 day at least.',
-    }),
+    })
+    .transform((intervals) => {
+      return intervals.map((interval) => {
+        return {
+          weekDay: interval.weekDay,
+          startTimeInMinutes: convertTimeStringToMinutes(interval.startTime),
+          endTimeInMinutes: convertTimeStringToMinutes(interval.endTime),
+        }
+      })
+    })
+    .refine(
+      (intervals) => {
+        return intervals.every(
+          (interval) =>
+            interval.startTimeInMinutes - 60 >= interval.endTimeInMinutes,
+        )
+      },
+      {
+        message: 'The period need to be bigger or equal to start time.',
+      },
+    ),
 })
 
-type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>
+type TimeIntervalsFormInput = z.input<typeof timeIntervalsFormSchema>
+type TimeIntervalsFormOutput = z.infer<typeof timeIntervalsFormSchema>
 
 export default function TimeIntervals() {
   const {
@@ -45,7 +67,7 @@ export default function TimeIntervals() {
     handleSubmit,
     control,
     formState: { isSubmitting, errors },
-  } = useForm({
+  } = useForm<TimeIntervalsFormInput>({
     resolver: zodResolver(timeIntervalsFormSchema),
     defaultValues: {
       intervals: [
@@ -65,7 +87,10 @@ export default function TimeIntervals() {
   })
   const weekdays = getWeekDays()
 
-  async function handleSetTimeIntervals(data: TimeIntervalsFormData) {}
+  async function handleSetTimeIntervals(data: any) {
+    const formData = data as TimeIntervalsFormOutput
+    console.log(formData)
+  }
 
   return (
     <Container>

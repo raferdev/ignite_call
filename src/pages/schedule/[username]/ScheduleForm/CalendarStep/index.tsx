4d3a145480be9d475cd8742b6e7ea3f52a55812c
Rafael <rafinha_fern@hotmail.com>
2023-04-04 20:43:38 -0300

    feat: new timer picker styles on calendar step page.

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,40 @@
 import { Calendar } from '@/components/Calendar/index.jsx'
-import { Container } from './styles.js'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles.js'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>20 de setembro</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }

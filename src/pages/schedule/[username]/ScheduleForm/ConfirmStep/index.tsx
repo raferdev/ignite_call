4d9c3f719d07589eb19e178b7195f6cd3a8e2107
Rafael <rafinha_fern@hotmail.com>
2023-04-04 22:37:43 -0300

    feat: confirm schedule page content and styles.

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,3 +1,39 @@
+import { CalendarBlank, Clock } from '@phosphor-icons/react'
+import { ConfirmForm, FormActions, FormHeader } from './styles.js'
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+
 export function ConfirmStep() {
-  return <div></div>
+  function handleConfirmScheduling() {}
+  return (
+    <ConfirmForm onSubmit={handleConfirmScheduling} as="form">
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Set 2023
+        </Text>
+        <Text>
+          <Clock />
+          18:00 h
+        </Text>
+      </FormHeader>
+      <label>
+        <Text size="sm">Full name</Text>
+        <TextInput placeholder="Your name" />
+      </label>
+      <label>
+        <Text size="sm">Full name</Text>
+        <TextInput type="email" placeholder="johndoe@exemple.com" />
+      </label>
+      <label>
+        <Text size="sm">Obs</Text>
+        <TextArea />
+      </label>
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancel
+        </Button>
+        <Button type="submit">Confirm</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
 }

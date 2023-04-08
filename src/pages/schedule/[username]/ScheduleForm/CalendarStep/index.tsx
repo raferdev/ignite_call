5577e85c642433b3bb4e30aed7bfe953d08ede6f
Rafael <rafinha_fern@hotmail.com>
2023-04-08 17:36:02 -0300

    feat: schedule model and calendar step page with the days formatted.

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -7,19 +7,22 @@ import {
   TimePickerList,
 } from './styles.js'
 import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
 
   const isDateSelected = !!selectedDate
 
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const describedDate = dayjs(selectedDate).format('DD[ of ]MMMM')
   return (
     <Container isTimePickerOpen={isDateSelected}>
       <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            terça-feira <span>20 de setembro</span>
+            {weekDay} <span>{describedDate}</span>
           </TimePickerHeader>
           <TimePickerList>
             <TimePickerItem>08:00</TimePickerItem>

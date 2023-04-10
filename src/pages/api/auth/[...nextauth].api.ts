d976e5c546ca707d97ec37ca8720d673726d9969
Rafael <rafinha_fern@hotmail.com>
2023-04-10 02:47:53 -0300

    chore: google api token refresh always return.

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -16,6 +16,9 @@ export function buildNextAuthOptions(
 
         authorization: {
           params: {
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
             scope:
               'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
           },

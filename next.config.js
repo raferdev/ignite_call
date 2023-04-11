043a2d1712ca7479c716b8c0410d8c9349eac0b0
Rafael <rafinha_fern@hotmail.com>
2023-04-11 16:09:20 -0300

    fix: phospor react config to work on vercel.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  modularizeImports: {
+    '@phosphor-icons/react': {
+      transform: '@phosphor-icons/react/dist/icons/{{member}}',
+    },
+  },
 }
 
 module.exports = nextConfig
